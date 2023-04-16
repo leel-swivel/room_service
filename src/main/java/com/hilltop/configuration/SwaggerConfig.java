f1658c4fa1514964f90cfe6de17463f0cd702fb1
leel-swivel <leel@swivelgroup.com.au>
2023-04-17 02:02:16 +0530

    Change swagger config

--- src/main/java/com/hilltop/configuration/SwaggerConfig.java
+++ src/main/java/com/hilltop/configuration/SwaggerConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,6 +24,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(generateAPIInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 //Here adding base package to scan controllers. This will scan only controllers inside
                 //specific package and include in the swagger documentation
@@ -31,12 +36,29 @@ public class SwaggerConfig {
 
     //Api information
     private ApiInfo generateAPIInfo() {
-        return new ApiInfo("Room Service", "Implementing Swagger with SpringBoot Application", "1.0.0",
+
+        return new ApiInfo("Hotel Service", "Implementing Swagger with SpringBoot Application", "1.0.0",
                 "", getContacts(), "", "", new ArrayList<>());
     }
 
     // Developer Contacts
     private Contact getContacts() {
         return new Contact("Leel K", "", "leel@swivelgroup.com.au");
+    }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
